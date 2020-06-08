dfa45f42ed9871d717ecd190ae4a32d2bfc86e7c
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-08 23:52:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,29 @@
 class Triangle
-
-  attr_reader :side1, :side2, :side3
-  
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end
-  def kind
-    valid_triangle
-    if side1 == side2 && side2 == side3
-      :equilateral
-    elsif side1 == side2 || side2 == side3 || side1 == side3
-      :isosceles
-    else
-      :scalene
-    end
-
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
-  def valid_triangle
-    if side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1 + side2 <= side3) || (side1 + side3 <= side2 || (side2 + side3 <= side1))
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
-  end
-  class TriangleError < StandardError
 
   end
+
+end
+
+class TriangleError < StandardError
+  # triangle error code
+end
